61b590f44cb50e50e803637b9ef9faddbab013da
Kevin Cartwright <kevincart@gmail.com>
2017-05-04 11:34:48 +1200

    “Added environment variable handler; google api calls consumed by index.ejs correctly!”

--- routes/gcall.js
+++ routes/gcall.js
@@ -1,14 +1,23 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
-var apiKey = process.env.apiKey;
+var apiKey = process.env.API_KEY;
 
 router.get('/gcall', function(req, res) {
   gps = req.query.coordinates;
   console.log(gps);
   var url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + gps
-    + "&radius=500&type=restaurant&keyword=cruise&key=" + apiKey;
+    + "&radius=1000&types=food&key=" + apiKey;
   console.log(url);
-  res.send("Async TEST" + url);
+
+  request(url, function (error, response, body) {
+    if (error) {
+      console.log('error: ', error); // handle error
+    } else {
+      console.log('status code: ', response && response.statusCode); // print response status code if response received
+      res.send(body);
+    }
+  });
 });
 
 module.exports = router;
